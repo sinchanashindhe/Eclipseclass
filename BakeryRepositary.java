e6f440f678207f75bbdd123eef999632006915ee
sinchanashindhe <113296319+sinchanashindhe@users.noreply.github.com>
2022-12-20 00:39:24 +0530

    Add files via upload

---
+++ BakeryRepositary.java
@@ -0,0 +1,14 @@
+package repositary;
+
+
+
+
+	import com.source.dto.BakeryDTO;
+	import com.source.exception.SizeIsFullExceptionInitiated;
+
+	public interface BakeryRepositary {
+
+
+		boolean saving(BakeryDTO dto) throws SizeIsFullExceptionInitiated;
+	}
+}
---
+++ BakeryRepositaryImplementation.java
@@ -0,0 +1,27 @@
+package repositary;
+
+
+	import com.source.dto.BakeryDTO;
+	import com.source.exception.SizeIsFullExceptionInitiated;
+
+	public class BakeryRepositaryImplementation implements BakeryRepositary {
+
+		private BakeryDTO[] dtos = new BakeryDTO[5];
+		private int index = 0;
+
+		@Override
+		public boolean saving(BakeryDTO dto) throws SizeIsFullExceptionInitiated {
+			System.out.println("Checking the repositary condition");
+			if(this.index>=this.dtos.length)
+			{
+				System.err.println("Exception is intitiated");
+				throw new SizeIsFullExceptionInitiated("Check the array size you trying to add additonal data");
+			}
+
+			dtos[index]=dto;
+			index++;
+			return false;
+		}
+
+	}
+}
