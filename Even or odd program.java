69a01dd5fd7d858c097d376f5d258e7be7c048ad
sinchanashindhe <113296319+sinchanashindhe@users.noreply.github.com>
2023-01-06 13:38:20 +0530

    Add files via upload

---
+++ Even or odd program.java
@@ -0,0 +1,21 @@
+public class EvenOrOdd{
+
+ public static void main(String[] args) 
+    Scanner sc = new scanner (System.in);
+	 
+	 
+	 int x = 0;
+	 System.out.println("enter an flower");
+	 sc.nextInt();
+	 if(x%10==){
+	System.out.println("even number is petals ");
+	 }
+	 else{
+		System.out.println("odd number is petals ");
+	 }
+}
+	 }
+
+
+
+
